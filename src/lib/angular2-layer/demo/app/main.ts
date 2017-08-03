67d7390d216aeacb964acb27e292773649a1a235
Baldwin <wei.li1@hitachiconsulting.com>
2017-08-03 17:21:19 +0800

    save layer and combox defect fixed
    save layer and combox defect fixed

---
+++ src/lib/angular2-layer/demo/app/main.ts
@@ -0,0 +1,77 @@
+import "zone.js";
+import "reflect-metadata";
+
+import {Component, NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/Forms';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {NgLayer, NgLayerRef, NgLayerComponent} from "../../angular2-layer";
+
+@Component({
+	selector: '.app',
+	templateUrl: 'temp/app.html',
+	providers: [NgLayer]
+})
+export class AppComponent {
+	constructor(private ly:NgLayer) {}
+	
+	config:any = {
+		inSelector:"fallDown",
+		outSelector:"rollOut",
+		title:"angular2 layer",
+		align:"top",
+		parent: this,
+		dialogComponent:DialogComponent,
+		closeAble: false
+	}
+	
+	dialog(){
+		this.ly.dialog(this.config);
+	}
+	
+	alert(){
+		this.ly.alert(this.config);
+	}
+	
+	confirm(){
+		this.ly.confirm(this.config);
+	}
+	
+	loading(){
+		let tip = this.ly.loading(this.config);
+		
+		setTimeout(()=>{tip.close();}, 2000)
+	}
+	
+	tip(){
+		this.ly.tip(this.config);
+	}
+}
+
+/*component for dialog*/
+@Component({
+	selector: '.dialog',
+	templateUrl: 'temp/dialog.html'
+})
+export class DialogComponent {
+	data = "angular2 layer";
+	
+	constructor(private ly:NgLayerRef, private l:NgLayer) {}
+	
+	setTitle(){this.ly.setTitle("Angular2 Layer Title");}
+	
+	close(){this.ly.close();}
+	
+	showCloseBtn(){this.ly.showCloseBtn(true)};
+	
+	showData(){alert(this.data)};
+}
+
+@NgModule({
+	imports: [BrowserModule, FormsModule],
+	entryComponents:[NgLayerComponent, DialogComponent],
+	declarations: [AppComponent, NgLayerComponent, DialogComponent],
+	bootstrap: [AppComponent]
+})
+class AppModule {}
+platformBrowserDynamic().bootstrapModule(AppModule);
